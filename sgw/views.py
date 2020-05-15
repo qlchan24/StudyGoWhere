7c342ca85d2753b79188462db862c7073b67b362
desmondaw <gimper.ds@gmail.com>
2020-05-15 21:13:55 +0800

    2nd attempt to add index.html for locations

--- sgw/views.py
+++ sgw/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Location
+
 # Create your views here.
-def index(request): 
-    return HttpResponse("Locations")
+def index(request):
+    context = { 
+        "locations": Location.objects.all()
+    }
+    return render(request, "sgw/index.html", context)
 
 def home(request): 
     return HttpResponse("home")
