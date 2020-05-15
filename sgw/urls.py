7b3fe73bde347c54b7b71fc6262a39b29d8307c2
desmondaw <gimper.ds@gmail.com>
2020-05-15 11:34:42 +0800

    suicide

--- sgw/urls.py
+++ sgw/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from sgw import views
 
+app_name = 'sgw'
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.home, name = "home")
+    path("locations", views.index, name="list-of-locations")
 ]
--- web_project/urls.py
+++ web_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("sgw.urls")),
+    path('sgw/', include('sgw.urls')),
 ]
