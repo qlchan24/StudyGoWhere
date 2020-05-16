8f660c859bf4b2e6c916eb68b9bfbf879d26b038
desmondaw <gimper.ds@gmail.com>
2020-05-16 16:04:32 +0800

    removed Location, Level and added Studyspot in models.py

--- sgw/models.py
+++ sgw/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-
-class Level(models.Model):
-    number = models.IntegerField()
-    crowdedness = models.IntegerField()
+class StudySpot(models.Model): 
+    crowdednessRating = models.IntegerField()
     airConditioned = True
     discussionFriendly = True
     wallSockets = True
 
+    levelNumber = models.IntegerField()
+    locationName = models.CharField(max_length=100)
 
-class Location(models.Model):
-    name = models.CharField(max_length=100)
-    levels = models.ForeignKey(Level, on_delete=models.CASCADE)
+    closingTime = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.name}"
+
+
+    
+
+
 
