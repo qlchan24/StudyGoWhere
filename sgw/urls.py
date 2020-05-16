3b65b75e64fb87d8347a283365d6c426d961ba2e
desmondaw <gimper.ds@gmail.com>
2020-05-16 17:42:51 +0800

    not much was done

--- sgw/urls.py
+++ sgw/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from sgw import views
+from . import views
 
 app_name = 'sgw'
 urlpatterns = [
     path("", views.home, name="home"),
     path("locations", views.index, name="list-of-locations"),
+    # path("studyspot-contribution", views.contribution, name = "studyspot-contribution-page")
 ]
--- sgw/views.py
+++ sgw/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Location
+from .models import StudySpot
 
 # Create your views here.
 def index(request):
     context = { 
-        "locations": Location.objects.all()
+        "studyspots": StudySpot.objects.all()
     }
     return render(request, "sgw/index.html", context)
 
 def home(request): 
     return HttpResponse("home")
+
+#def contribution(request): 
+#    return 
