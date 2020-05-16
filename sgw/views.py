8d8aee2851c1a3961a1f8f955017a1eb5c11444e
qlchan24 <qlchan24@gmail.com>
2020-05-16 17:52:39 +0800

    moved the template to the correct folder, updated requirements.txt,updated views.index

--- sgw/views.py
+++ sgw/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Location
+from .models import StudySpot
 
 # Create your views here.
+
+
 def index(request):
-    context = { 
-        "locations": Location.objects.all()
+    context = {
+        "locations": StudySpot.objects.order_by().values('locationName').distinct()
     }
     return render(request, "sgw/index.html", context)
 
-def home(request): 
+
+def home(request):
     return HttpResponse("home")
