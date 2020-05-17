7d553d06cefd3b7b113f5f3ddd70550653cd47b1
desmondaw <gimper.ds@gmail.com>
2020-05-17 15:59:55 +0800

    changed contributeStudySpotForm to ModelForm

--- sgw/forms.py
+++ sgw/forms.py
@@ -1,19 +1,25 @@
 from django import forms
+from django.forms import ModelForm
 from django.db import models
 from .models import StudySpot
 
-class ContributeStudySpotForm(forms.Form): 
-    description = forms.CharField(max_length=100)
-    class Meta:
+class ContributeStudySpotForm(ModelForm): 
+    class Meta: 
         model = StudySpot
-        widgets = {
-            'airConditioned': forms.RadioSelect, 
-            'discussionFriendly': forms.RadioSelect,
-            'wallSockets': forms.RadioSelect,
-        }
-    levelNumber = models.IntegerField()
-    locationName = models.CharField(max_length=100)
-    crowdednessRating = models.IntegerField()
+        fields = ['description','crowdednessRating','airConditioned','discussionFriendly',
+        'wallSockets','levelNumber','locationName','openingTime','closingTime']
 
-    closingTime = models.IntegerField()
+    # description = forms.CharField(max_length=100)
+    # class Meta:
+    #     model = StudySpot
+    #     widgets = {
+    #         'airConditioned': forms.RadioSelect, 
+    #         'discussionFriendly': forms.RadioSelect,
+    #         'wallSockets': forms.RadioSelect,
+    #     }
+    # levelNumber = models.IntegerField()
+    # locationName = models.CharField(max_length=100)
+    # crowdednessRating = models.IntegerField()
+
+    # closingTime = models.IntegerField()
     
