2f1bc6a792266173e41ce8c49907d68eacc7c055
desmondaw <gimper.ds@gmail.com>
2020-05-20 01:00:28 +0800

    changed settings.py, updated LocationContribute form so that it does not allow duplicate location names

--- sgw/views.py
+++ sgw/views.py
@@ -6,6 +6,8 @@ from .models import StudySpot, Rating, Location
 from .forms import ContributeStudySpotForm, ContributeRatingForm, ContributeLocationForm
 from django.urls import reverse
 
+from django.contrib import messages
+
 # Create your views here.
 
 
@@ -22,21 +24,22 @@ def home(request):
 
 def locationpage(request, location):
     loc = get_object_or_404(Location, locationName=location)
+    # loc = Location.objects.get(locationName=location)
     context = {
         "location": loc,
         "openingTime": loc.openingTime,
         "closingTime": loc.closingTime,
-        "levelNumber": list(StudySpot.objects.filter(Location=loc).values_list('levelNumber', flat=True).distinct()),
-        "description": list(StudySpot.objects.filter(Location=loc))
+        "levelNumber": list(StudySpot.objects.filter(locationName=loc).values_list('levelNumber', flat=True).distinct()),
+        "description": list(StudySpot.objects.filter(locationName=loc))
     }
     return render(request, "sgw/locationpage.html", context)
 
 
 def studyspotpage(request, location, studyspot):
-    studyspotquery = StudySpot.objects.filter(description=studyspot)
+    studyspotquery = StudySpot.objects.get(description=studyspot)
     context = {
         "studyspot": studyspot,
-        "ratings": list(Rating.objects.filter(studyspot=studyspotquery.first())),
+        "ratings": list(Rating.objects.filter(studyspot=studyspotquery)),
         "location": location,
     }
     return render(request, "sgw/studyspotpage.html", context)
@@ -69,9 +72,13 @@ def contributeLocation(request):
     if request.method == 'POST':
         form = ContributeLocationForm(request.POST)
         if form.is_valid():
-            location = form.save()
-            return HttpResponseRedirect(reverse('sgw:list-of-locations'))
-            # return render(request, 'sgw/index.html', {'studyspots':studyspots})  # not sure about this?
+            name = form.cleaned_data['locationName']
+            if Location.objects.filter(locationName=name).exists(): 
+                messages.error(request, "fdkfdhlkfeh") # error not showing
+                # return HttpResponseRedirect(reverse('sgw:location-contribution-page'))
+            else: 
+                location = form.save()
+                return HttpResponseRedirect(reverse('sgw:list-of-locations'))
     else:
         form = ContributeLocationForm()
     return render(request, 'contributeLocation.html', {'form': form})
